51a9b989b6fd4e86ce9fc496d916503f76af0233
hong9802 <hlee993@gmail.com>
2024-10-22 02:43:06 +0900

    Fix safeBrowsingAPI not working.

--- app/src/main/java/com/bread_cat/QRchan/RetrofitClient.kt
+++ app/src/main/java/com/bread_cat/QRchan/RetrofitClient.kt
@@ -1,14 +1,23 @@
-package com.bread_cat.QRchan
-
+import com.bread_cat.QRchan.SafeBrowsingService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://safebrowsing.googleapis.com"
+    private const val BASE_URL = "https://safebrowsing.googleapis.com/"
 
     fun getService(): SafeBrowsingService {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
--- app/src/main/java/com/bread_cat/QRchan/SafeBrowsingService.kt
+++ app/src/main/java/com/bread_cat/QRchan/SafeBrowsingService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 // Safe Browsing API Request/Response 모델 설정
 data class SafeBrowsingRequest(val client: ClientInfo, val threatInfo: ThreatInfo)
@@ -14,7 +15,10 @@ data class SafeBrowsingResponse(val matches: List<ThreatMatch>?)
 data class ThreatMatch(val threatType: String, val platformType: String, val threat: ThreatEntry)
 
 interface SafeBrowsingService {
-    @Headers("Content-Type: application/json")
-    @POST("/v4/threatMatches:find")
-    fun checkUrl(@Body request: SafeBrowsingRequest): Call<SafeBrowsingResponse>
+    @POST("v4/threatMatches:find")
+    fun checkUrl(
+        @Query("key") apiKey: String, // API 키를 쿼리 매개변수로 받습니다.
+        @Body request: SafeBrowsingRequest
+    ): Call<SafeBrowsingResponse>
 }
+
